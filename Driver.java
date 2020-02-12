e6699078b018a732430b7db7fe661962a68a536e
BlooStu2 <bloo@debian.bloostu2>
2020-02-12 18:30:54 +0000

    allahuakbar

--- Driver.java
+++ Driver.java
@@ -1,16 +1,37 @@
 public class Driver{
     public static void main(String []args){
-        GameArena mainArena = new GameArena(1000,1000);   
-        Ball player = new Ball(500,500,100,"BLUE");
+        GameArena mainArena = new GameArena(2000,2000);   
+        Ball player = new Ball(420,169,100,"BLUE");
         mainArena.addBall(player);
-        
+        boolean xEdge = true;
+        boolean YEdge = true; 
         while(true){
-            if(player.getXPosition()<1000){
+            if (xEdge == true){
                 player.setXPosition((player.getXPosition()+1));
             }
             else{
-                player.setXPosition(0);
+                player.setXPosition((player.getXPosition()-1));
             }
+            if (YEdge == true){
+                player.setYPosition((player.getYPosition()+1));
+            }
+            else{
+                player.setYPosition((player.getYPosition()-1));
+            }
+            if (player.getXPosition() == 100 || player.getXPosition() == 1900){
+                xEdge = !xEdge;
+            }
+            if (player.getYPosition() == 100 || player.getYPosition() == 1900){
+                YEdge = !YEdge;
+            }
+        try
+        {
+            Thread.sleep(1);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
         }
     }
 }
