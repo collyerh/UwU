ba992abad4da50fcd217be85cd36fc165e1a5aa1
Collyer-Hoar <collyerh@egb070000041.lancs.local>
2020-02-13 12:58:22 +0000

    Test1.java

---
+++ Test1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Test{
+    
+    public static void main(String []args){
+
+    GameArena mainArena = new GameArena(1000,1000);
+    Ball ball1 = new Ball(10,10,10,"YELLOW");
+    ball1.setSize(100);
+    ball1.setXPosition(500);
+    ball1.setYPosition(500);
+
+    mainArena.addBall(ball1);
+
+    while(true) {
+  
+
+
+
+
+
+    }
+
+    
+}
+}
