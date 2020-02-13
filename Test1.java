49aec1e0c71f10fce88fea4e99f23cf5303e7b81
Collyer-Hoar <collyerh@egb070000041.lancs.local>
2020-02-13 13:23:22 +0000

    name jeff

--- Test1.java
+++ Test1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Test{
+public class Test1{
     
     public static void main(String []args){
 
@@ -11,14 +11,20 @@ public class Test{
     ball1.setXPosition(500);
     ball1.setYPosition(500);
 
+    //Rectangle rectangle1 = new Rectangle(100,100,100,100,"RED");
+    //rectangle1.setSize(500,500);
+    //rectangle1.setXPosition(50);
+    //rectangle1.setYPosition(50);
+
     mainArena.addBall(ball1);
 
+    boolean hg = mainArena.spacePressed();
+
     while(true) {
-  
 
-
-
-
+    double current = ball1.getXPosition();
+    mainArena.pause();
+    ball1.setXPosition(current + 1);
 
     }
 
